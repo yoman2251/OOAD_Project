a49d25253e64ee7de42e851791711b4f5d643d95
jysh1214 <jyxemperor@gmail.com>
2019-03-02 13:32:43 +0800

    Add files via upload

---
+++ Meeting_List_System/Meeting_List_System/Pages/Register.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meeting_List_System.Pages
+{
+    public class RegisterModel : PageModel
+    {
+
+        public void OnGet()
+        {
+            //mode
+        }
+    }
+}
---
+++ Meeting_List_System/Meeting_List_System/Pages/ReportOrder.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meeting_List_System.Pages
+{
+    public class ReportOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
